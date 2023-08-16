b117595f69d2f7fe0a2e8de1bd1a1f07b07196f0
ArtemMakovskyy <photomak79@gmail.com>
2023-08-16 15:18:12 +0300

    create CustomGlobalExceptionHandler in exception folder, and made validation in CreateBookRequestDto

--- src/main/java/book_store/dto/CreateBookRequestDto.java
+++ src/main/java/book_store/dto/CreateBookRequestDto.java
@@ -1,16 +1,19 @@
 package book_store.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
-
 import java.math.BigDecimal;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank(message = "Title should not be blank")
     private String title;
+    @NotBlank(message = "Author should not be blank")
     private String author;
+    @NotBlank(message = "Isbn should not be blank")
     private String isbn;
+    @Positive(message = "Price should be positive")
     private BigDecimal price;
     private String description;
     private String coverImage;
