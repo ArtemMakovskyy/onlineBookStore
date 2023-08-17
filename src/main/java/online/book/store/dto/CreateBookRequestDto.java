40db97d967c711a757c95cc2007e5cc0ff33fc9d
ArtemMakovskyy <photomak79@gmail.com>
2023-08-17 21:58:55 +0300

    fixed mistakes

--- src/main/java/online/book/store/dto/CreateBookRequestDto.java
+++ src/main/java/online/book/store/dto/CreateBookRequestDto.java
@@ -1,7 +1,8 @@
 package online.book.store.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
@@ -13,7 +14,8 @@ public class CreateBookRequestDto {
     private String author;
     @NotBlank(message = "Isbn should not be blank")
     private String isbn;
-    @Positive(message = "Price should be positive")
+    @NotNull(message = "Please enter price ")
+    @Min(value = 0, message = "Invalid price, it cannot be less than zero")
     private BigDecimal price;
     private String description;
     private String coverImage;
