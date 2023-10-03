e3da196ac21c66d5674d3cc0ebe257911fbdf25f
ArtemMakovskyy <photomak79@gmail.com>
2023-10-03 15:47:44 +0300

    working with postgresDb with docker without liqubase

--- src/test/java/online/book/store/OnlineBookStore.java
+++ src/test/java/online/book/store/OnlineBookStore.java
@@ -1,11 +1,10 @@
 package online.book.store;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class OnlineBookStore {
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 }
