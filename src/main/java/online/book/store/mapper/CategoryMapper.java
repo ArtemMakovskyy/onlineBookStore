9baff9e15bd75d6a589a9b9537725deea7c506d3
ArtemMakovskyy <photomak79@gmail.com>
2023-10-04 17:24:23 +0300

    Implementation working Docker with MySQL and liqubase

--- src/main/java/online/book/store/mapper/CategoryMapper.java
+++ src/main/java/online/book/store/mapper/CategoryMapper.java
@@ -4,8 +4,9 @@ import online.book.store.config.MapperConfig;
 import online.book.store.dto.category.CategoryDto;
 import online.book.store.model.Category;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfig.class, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface CategoryMapper {
     CategoryDto toDto(Category category);
 
